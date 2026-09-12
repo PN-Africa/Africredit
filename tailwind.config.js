816ea84469a211a8a4557fa1592eb21d6fe398b6
user <user@users-MacBook-Pro.local>
2026-09-12 02:33:27 -0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
-  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
